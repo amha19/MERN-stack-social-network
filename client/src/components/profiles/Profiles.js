87d9c429e283c91c18ff06c3e3c9374b7b8c9ce5
amha19 <34722096+amha19@users.noreply.github.com>
2021-09-29 11:24:16 -0500

    add icons to profiles list

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,9 +2,24 @@ import React, { useEffect } from 'react';
 import { useGlobalContext } from '../../context/devsContext';
 import { getProfiles } from '../../context/actions/profile';
 import ProfileItem from './ProfileItem';
-import { LinearProgress, Typography, Grid } from '@material-ui/core';
+import {
+    LinearProgress,
+    Typography,
+    Grid,
+    makeStyles,
+    Box,
+} from '@material-ui/core';
+import SettingsInputSvideoIcon from '@material-ui/icons/SettingsInputSvideo';
+
+const useStyles = makeStyles((theme) => ({
+    header: {
+        margin: theme.spacing(3, 0, 1, 3),
+    },
+}));
 
 const Profiles = () => {
+    const classes = useStyles();
+
     const { profileDispatch, profiles, profileLoading } = useGlobalContext();
 
     useEffect(() => {
@@ -15,6 +30,21 @@ const Profiles = () => {
 
     return (
         <Grid container direction="column" alignContent="center">
+            <Typography color="primary" variant="h2" className={classes.header}>
+                Developers
+            </Typography>
+            <Box
+                className={classes.subHeader}
+                mb={1.5}
+                ml={3}
+                display="flex"
+                flexDirection="row"
+            >
+                <SettingsInputSvideoIcon style={{ marginRight: 8 }} />
+                <Typography variant="h5">
+                    Browse and connect with developers
+                </Typography>
+            </Box>
             {profiles.length > 0 ? (
                 profiles.map((profile) => (
                     <ProfileItem key={profile._id} profile={profile} />
